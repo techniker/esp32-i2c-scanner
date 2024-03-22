983136cfa6201f2b7503af82ac915efd9a38fe16
Bjoern Heller <tec@sixtopia.net>
2024-03-23 00:53:44 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+//Simple i2c bus scanner
+//Bjoern Heller <tec att sixtopia.net>
+
+#include <Arduino.h>
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(115200); // Start the Serial communication
+  delay(5000); // This delay gives time to open the Serial Monitor after uploading the code
+  Wire.begin(); // Initialize the I2C communication
+
+  Serial.println("\nI2C Scanner");
+}
+
+void loop() {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for (address = 1; address < 127; address++) {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000); // Wait 5 seconds for the next scan
+}
